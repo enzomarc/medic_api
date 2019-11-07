fcda8085a87203a7879c27d649534154c7a41779
Marc Enzo <emarc237@gmail.com>
2019-11-07 03:58:11 +0100

    perf(core): enable facades and eloquent
    Enable facades and eloquent to facilitate access to some classes.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+	return "Medic API v1";
+});
+
+Route::group('api', function () {
+	
 });
